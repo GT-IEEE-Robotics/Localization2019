ea72273bd3f6da5cc434c4db13cfce7720e12058
Tony Wineman <tonywineman@gmail.com>
2019-04-04 20:40:44 -0400

    Updated the range checking for the ptOnLine function and added some comments that make understanding it managable.

--- ransac.py
+++ ransac.py
@@ -1,11 +1,7 @@
-#input polarCord as a list of tuples with form (radius, theta)
-#outputs a list of tuples with the form of (x, y)
 import math
 import random
 import matplotlib.pyplot as plt
-
-SIDE_LENGTH = 3000
-ERROR = 0.025
+from random_square import random_square
 
 def cartesianConvert(polarCord):
     out = []
@@ -13,32 +9,12 @@ def cartesianConvert(polarCord):
         out.append((point[0] * math.cos(point[1]), point[0] * math.sin(point[1])))
     return out
 
+
 def getDist(pt1, pt2):
     out = math.sqrt(math.pow(pt2[0] - pt1[0], 2) + math.pow(pt2[1] - pt1[1], 2))
     # print(out)
     return out
 
-#take in 2 points as tuples (x,y)
-#outputs a 2 tuples for lines that have the form y = mx + b [(m1, b1), (m2, b2)]
-#these are the two lines that go from point 1 to form half a square 
-# def drawSquare(pt1, pt2):
-#     if pt1[0] > pt2[0]:     #draw the same square regardless of order of points
-#         tmp = pt1
-#         pt1 = pt2
-#         pt2 = tmp
-#     if pt2[0] - pt1[0] == 0:  #need to deal with vertical lines
-#         m1 = (pt2[1] - pt1[1]) / 0.00000000001
-#     else:
-#         m1 = (pt2[1] - pt1[1]) / (pt2[0] - pt1[0])
-#     if m1 == 0:
-#         m2 = -1 / 0.00000000001
-#     else:
-#         m2 = -1 / m1
-#     b1 = pt1[1] - m1 * pt1[0]
-#     b2 = pt1[1] - m2 * pt1[0]
-#     b3 = pt2[1] - m1 * pt2[0] 
-#     print([(m1, b1), (m2, b2)])
-#     return [(m1, b1), (m2, b2)]
 
 def drawSquare(pt1, pt2):       #https://www.quora.com/Given-two-diagonally-opposite-points-of-a-square-how-can-I-find-out-the-other-two-points-in-terms-of-the-coordinates-of-the-known-points
     A = pt1
@@ -51,42 +27,81 @@ def drawSquare(pt1, pt2):       #https://www.quora.com/Given-two-diagonally-oppo
     return [A, B, C, D]
 
 
-def ptOnLine(pt1, pt2, ptCheck, error):      #returns 1 if on line, otherwise 0
-    out = 0
+#pt1 is a tuple (x, y) that represents one point that makes up the line
+#pt2 is a tuple (x, y) that represents one point that makes up the line
+#ptcheck is a tuple (x,y) that we are check to see if it is on the line
+#error is the acceptable percent error based on the side length to find if points lie in (range of 0 to 1)
+#returns 1 if on line, otherwise 0
+def ptOnLine(pt1, pt2, ptCheck, error):
     side = getDist(pt1, pt2)
-    if pt1[0] > pt2[0]:       #making pt1 always to the left of pt2
+    error_dist = side * error
+
+    #making pt1 always to the left of pt2 for proper range checking
+    if pt1[0] > pt2[0]:
         tmp = pt1
         pt1 = pt2
         pt2 = tmp
-    if pt1[0] - (pt2[0] - pt1[0]) * error <= ptCheck[0] <= pt2[0] + (pt2[0] - pt1[0]) * error:    #check if x val is within range
-        if pt2[0] - pt1[0] != 0:
-            m = (pt2[1] - pt1[1]) / (pt2[0] - pt1[0])
-            b = pt1[1] - m * pt1[0]
-            yOnLine = m * ptCheck[0] + b
-            if m != 0:
-                error_dist = side * error
-                denom = math.cos(math.atan(1 / m))
-                if denom == 0:
-                    out = 1
-                else:
-                    if yOnLine - error_dist / denom <= ptCheck[1] <= yOnLine + error_dist / denom:
-                        out = 1
-            else:
-                if yOnLine - side * error <= ptCheck[1] <= yOnLine + side * error:
-                    out = 1
+        
+    #preparing stuff to check if x is in range
+    #checking if it is not a vertical line
+    if pt2[0] - pt1[0] != 0:
+        m = (pt2[1] - pt1[1]) / (pt2[0] - pt1[0])
+    else:
+        m = (pt2[1] - pt1[1]) / .0000000001
+    
+    # print(m)
+    #if the slope is less then 1 then the line limiting the x limit is the line perpendicular to our line
+    if m < 1:
+        if m == 0:
+            m = .0000000001
+        m = -1 / m
+
+    denom = math.cos(math.atan(1 / m))
+
+    #check if x val is within range finally
+    if pt1[0] - error_dist / denom <= ptCheck[0] <= pt2[0] + error_dist / denom:
+        #checking if y is in overall range if pt1's y val is less than pt2's y val and vise versa
+        yInRange = 0
+        if pt1[1] < pt2[1]:
+            if pt1[1] - error_dist / denom <= ptCheck[1] <= pt2[1] + error_dist / denom:
+                yInRange = 1
         else:
-            out = 1
-    # print(out)
-    return out
+            if pt2[1] - error_dist / denom <= ptCheck[1] <= pt1[1] + error_dist / denom:
+                yInRange = 1
+
+        if yInRange:
+            #checking if it is not a vertical line and not a horizontal line
+            if pt2[0] - pt1[0] != 0 and pt2[1] - pt1[1] != 0:
+                m = (pt2[1] - pt1[1]) / (pt2[0] - pt1[0])
+                b = pt1[1] - m * pt1[0]
+                yOnLine = m * ptCheck[0] + b
+                if m < 1:
+                    m = -1 / m
+                if m != 0:
+                    denom = math.cos(math.atan(1 / m))
+                    if denom == 0:
+                        return 1
+                    else:
+                        if yOnLine - error_dist / denom <= ptCheck[1] <= yOnLine + error_dist / denom:
+                            return 1
+                else:
+                    if yOnLine - side * error <= ptCheck[1] <= yOnLine + side * error:
+                        return 1
+                        
+            #if its a vertical line or a horizontal line and the x is in range and y is in range then it is good
+            else:
+                return 1
+    return 0
 
 
 
 
 #cartesianCord is a list of tuples of (x, y)
-#sideLength is the known length of the square be localized
-#error is the acceptable percent error of the drawn square to find if points lie in
-#percentOfPoints is the percentage of points that need to be included in the square to return a successful value
-#outputs a tuple where the first value is 0 if failed or 1 if success
+#sideLength is the known length of the side of the square be approximated
+#error is the acceptable percent error based on the side length to find if points lie in (range of 0 to 1)
+#percentOfPoints is the percentage of points that need to be included in the square to return a successful value (range of 0 to 1)
+#numIterations is the number of pairs of points it will check
+#outputs the verticies of the square on success or a zero on failure
 def ransacSquare(cartesianCord, sideLength, error, percentOfPoints, numIterations):
     # for i in range(len(cartesianCord)):
     #     x = cartesianCord[i][0]
@@ -94,27 +109,38 @@ def ransacSquare(cartesianCord, sideLength, error, percentOfPoints, numIteration
     #     plt.plot(x,y, marker='o', markersize=3, color="red")
 
     iteration = 0
-    success = 0
-    while not success and iteration < numIterations:
+    random.seed(2)
+    while iteration < numIterations:
         pt1 = cartesianCord[random.randrange(len(cartesianCord))]
         # print(pt1)
         pt2 = cartesianCord[random.randrange(len(cartesianCord))]
         # print(pt2)
-        if sideLength * (1 - error) * math.sqrt(2) < getDist(pt1, pt2) < sideLength * (1 + error) * math.sqrt(2):   #don't bother checking points that are right next to each other
+
+        #don't bother checking points that are right next to each other and wont form a big enough square
+        if sideLength * (1 - error) * math.sqrt(2) < getDist(pt1, pt2) < sideLength * (1 + error) * math.sqrt(2):
             verts = drawSquare(pt1, pt2)
-            for i in range(len(cartesianCord)):
-                x = cartesianCord[i][0]
-                y = cartesianCord[i][1]
-                plt.plot(x,y, marker='o', markersize=3, color="red")
-            plt.plot((verts[0][0], verts[1][0], verts[2][0], verts[3][0], verts[0][0]), (verts[0][1], verts[1][1], verts[2][1], verts[3][1], verts[0][1]), color="blue")
-            plt.show()
+
+            #plotting all the points and the square
+            # for i in range(len(cartesianCord)):
+            #     x = cartesianCord[i][0]
+            #     y = cartesianCord[i][1]
+            #     plt.plot(x,y, marker='o', markersize=3, color="red")
+            # plt.plot((verts[0][0], verts[1][0], verts[2][0], verts[3][0], verts[0][0]), (verts[0][1], verts[1][1], verts[2][1], verts[3][1], verts[0][1]), color="blue")
+            # plt.show()
+
             #now i need to count the number of points that are on the new square
             count = 0
             for point in cartesianCord:
                 if ptOnLine(verts[0], verts[1], point, error) or ptOnLine(verts[1], verts[2], point, error) or ptOnLine(verts[2], verts[3], point, error) or ptOnLine(verts[0], verts[3], point, error):
                     count = count + 1
-            if count >= percentOfPoints * len(cartesianCord):
-                return verts
+                    # print(count)
+                    plt.plot(point[0], point[1], marker='o', markersize=3, color="green")
+                else:
+                    plt.plot(point[0], point[1], marker='o', markersize=3, color="red")
+                if count >= percentOfPoints * len(cartesianCord):
+                    plt.plot((verts[0][0], verts[1][0], verts[2][0], verts[3][0], verts[0][0]), (verts[0][1], verts[1][1], verts[2][1], verts[3][1], verts[0][1]), color="blue")
+                    plt.show()
+                    return verts
         iteration = iteration + 1
     return 0
 
@@ -130,13 +156,17 @@ def main():
     #     plt.plot(x,y, marker='o', markersize=3, color="red")
     # plt.show()
 
-    print(ransacSquare(lidar_coords, 100, .04, .97, 1000))
+    print(ransacSquare(lidar_coords, 100, .07, .8, 100000))
         
 
 
 
 # getDist((1,0), (1,1))
 # drawSquare((0,0), (1,2))
-# ptOnLine((0,0), (1,0), (.5,-.025), .05)
-main()
+# print(ptOnLine((0,0), (-1,2), (-0.6,1), .05))
+# print("-------")
+# print(ptOnLine((0,0), (0,1.000000001), (0, 4), .05))# does not check initial y ranges properly
+# main()
+# print(len(random_square(100, 5)))
+print(ransacSquare(random_square(100, 1), 100, .025, .95, 100000))
 
